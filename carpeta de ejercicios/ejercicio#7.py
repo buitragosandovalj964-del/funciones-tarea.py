9cd70430aee5aa5c384251f052feb6381a0d78fa
buitragosandovalj964-del <buitragosandovalj964@gmail.com>
2026-01-28 11:21:25 -0500

    dejo solo funciones-ejercicio2

--- carpeta de ejercicios/ejercicio#7.py
+++
@@ -1,8 +0,0 @@
-#Escribe un programa en Python que convierta
-#  kilómetros a millas utilizando el factor 0.621371, 
-# elige un valor en kilómetros (por ejemplo, 10), y muestre en la consola el valor original y el convertido.
-kilometros = 10
-factor_conversion = 0.621371
-millas = kilometros * factor_conversion
-print(f"Valor original: {kilometros} km")
-print(f"Valor convertido: {millas} mi")
